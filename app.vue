0247eae3a357a6b4fab476545f47df34fb70c5cd
bnar98 <bnar.paskandy@gmail.com>
2024-07-16 15:10:04 +0300

    tt

--- app.vue
+++ app.vue
@@ -14,13 +14,17 @@
 useHead({
   title: "My App",
   meta: [
-    { name: "og:image", content: "https://www.tiwarnews.net/logo.png" },
+    {
+      name: "og:image",
+      content:
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+    },
     {
       name: "og:description",
       content:
         "social-links is a set of social media share links working with React and Svelte",
     },
-    { name: "og:title", content: "My amazing site." },
+    { name: "og:title", content: "My amazing site. bnarrr" },
   ],
 });
 const url = "https://nuxt-test-lovat-three.vercel.app/";
